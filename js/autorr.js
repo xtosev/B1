9a9c07583c02d83e19c98dd77056d6c67a0eca9e
Danilo Tosev <danilo.tosev@tutanota.com>
2024-05-30 20:22:52 +0200

    Initial commit

---
+++ js/autorr.js
@@ -0,0 +1,7 @@
+document.getElementById("uputstvo").addEventListener("click", ()=>{
+    window.location.href = "uputstvo.html"
+})
+
+document.getElementById("pocetna").addEventListener("click", ()=>{
+    window.location.href = "fotogalerija.html"
+})
---
+++ js/upu.js
@@ -0,0 +1,7 @@
+document.getElementById("pocetna").addEventListener("click", ()=>{
+    window.location.href = "fotogalerija.html"
+})
+
+document.getElementById("autor").addEventListener("click", ()=>{
+    window.location.href = "oautoru.html"
+})
